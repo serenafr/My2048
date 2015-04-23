52fa748699761bf539980b0fa135967485fd6dfd
serenafr <angneszhy@gmail.com>
2015-04-23 12:25:01 -0700

    Put different moduals into different functions; Add Key Event to UP, DOWN, LEFT and RIGHT Buttons.

--- src/main_window.py
+++ src/main_window.py
@@ -3,117 +3,173 @@ import wx.lib.stattext as ST
 import board
 
 class My2048_wx(wx.Frame):
+	SIZE = 4
+
 	def __init__(self, parent, id, title, size, board_object):
 		super(My2048_wx, self).__init__(parent, title = title, 
 			size = size)
-		board_object = board.Board
+		self.board_object = board_object
+		'''This list is used to store the wx labels with information
+		ST.GenStaticText(self, -1, label = text_list[i])
+		And put all the numbers from the board into the GUI'''
+		self.label_list = []
 		self.Construct()
 
 	def Construct(self):
-		SIZE = 4;
-
 		'''panel_box is the container that contains all the widgets'''
-		panel_box = wx.BoxSizer(wx.VERTICAL)
+		self.panel_box = wx.BoxSizer(wx.VERTICAL)
 
+		self.generate_header()
+
+		'''play_board is a container where all the tiles are put '''
+		self.play_board = wx.GridSizer(self.SIZE, self.SIZE)
+		self.generate_playboard()
+
+		'''User can use these keys to control the move and merge of the tile numbers'''
+		self.ctrl_keys = wx.BoxSizer(wx.VERTICAL)
+		self.generate_ctrlkeys()
+
+		self.SetSizer(self.panel_box)
+		self.Show(True)
+
+
+	def generate_header(self):
 		'''header is the top parts which holds the name of the game,
 		current score, and the best score'''
-		header = wx.BoxSizer(wx.VERTICAL)	
+		self.header = wx.BoxSizer(wx.VERTICAL)	
 
 		'''upper_header contains three parts: game_name(2048), a boxsizer contains the current score information
 		and another boxsizer contains the best score informaton
 		All three parts are lined HORIZONTAL'''
-		upper_header = wx.BoxSizer(wx.HORIZONTAL)
+		self.upper_header = wx.BoxSizer(wx.HORIZONTAL)
 	
-		game_name = ST.GenStaticText(self, -1, label = '2048', 
+		self.game_name = ST.GenStaticText(self, -1, label = '2048', 
 			size = (100, 30), style = wx.ALIGN_CENTRE)
-		upper_header.Add(game_name, flag = wx.EXPAND|wx.RIGHT, border = 60)
+		self.upper_header.Add(self.game_name, flag = wx.EXPAND|wx.RIGHT, border = 60)
 
-		upper_header_score = wx.BoxSizer(wx.VERTICAL)
-		score_str = ST.GenStaticText(self, -1, label = 'SCORE', size = (50, 20), style = wx.ALIGN_CENTRE)
-		score_str.SetBackgroundColour((187, 173, 160))
-		score = ST.GenStaticText(self, -1, label = '0', size = (50, 20), style = wx.ALIGN_CENTRE)
-		score.SetForegroundColour('white')
-		score.SetBackgroundColour((187, 173, 160))
-		upper_header_score.AddMany([score_str, score])
-		upper_header.Add(upper_header_score, flag = wx.EXPAND|wx.LEFT|wx.RIGHT, border = 10)
+		self.upper_header_score = wx.BoxSizer(wx.VERTICAL)
+		self.score_str = ST.GenStaticText(self, -1, label = 'SCORE', size = (50, 20), style = wx.ALIGN_CENTRE)
+		self.score_str.SetBackgroundColour((187, 173, 160))
+		self.score = ST.GenStaticText(self, -1, label = '0', size = (50, 20), style = wx.ALIGN_CENTRE)
+		self.score.SetForegroundColour('white')
+		self.score.SetBackgroundColour((187, 173, 160))
+		self.upper_header_score.AddMany([self.score_str, self.score])
+		self.upper_header.Add(self.upper_header_score, flag = wx.EXPAND|wx.LEFT|wx.RIGHT, border = 10)
 
-		upper_header_best = wx.GridSizer(2, 1)
-		best_str = ST.GenStaticText(self, -1, label = 'BEST', size = (50, 20), style = wx.ALIGN_CENTRE)
-		best_str.SetBackgroundColour((187, 173, 160))
-		best = ST.GenStaticText(self, -1, label = '0', size = (50, 20), style = wx.ALIGN_CENTRE)
-		best.SetForegroundColour('white')
-		best.SetBackgroundColour((187, 173, 160))
-		upper_header_best.AddMany([best_str, best])
-		upper_header.Add(upper_header_best)
+		self.upper_header_best = wx.GridSizer(2, 1)
+		self.best_str = ST.GenStaticText(self, -1, label = 'BEST', size = (50, 20), style = wx.ALIGN_CENTRE)
+		self.best_str.SetBackgroundColour((187, 173, 160))
+		self.best = ST.GenStaticText(self, -1, label = '0', size = (50, 20), style = wx.ALIGN_CENTRE)
+		self.best.SetForegroundColour('white')
+		self.best.SetBackgroundColour((187, 173, 160))
+		self.upper_header_best.AddMany([self.best_str, self.best])
+		self.upper_header.Add(self.upper_header_best)
 		
-		header.Add(upper_header)
+		self.header.Add(self.upper_header)
 
 		'''lower_header contains a sub_title and a button that allows users to start a new game'''
-		lower_header = wx.BoxSizer(wx.HORIZONTAL)
+		self.lower_header = wx.BoxSizer(wx.HORIZONTAL)
 
-		sub_title = ST.GenStaticText(self, -1, label = 'Join the numbers and get to the 2048 tile!')
-		lower_header.Add(sub_title, flag = wx.EXPAND|wx.RIGHT, border = 5)
+		self.sub_title = ST.GenStaticText(self, -1, label = 'Join the numbers and get to the 2048 tile!')
+		self.lower_header.Add(self.sub_title, flag = wx.EXPAND|wx.RIGHT, border = 5)
 
-		new_game_button = wx.Button(self, -1, label = 'NEW GAME')
-		lower_header.Add(new_game_button)
+		self.new_game_button = wx.Button(self, -1, label = 'NEW GAME')
+		self.lower_header.Add(self.new_game_button)
 
-		header.Add(lower_header)
+		self.header.Add(self.lower_header)
 
-		panel_box.Add(header, flag = wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM, 
+		self.panel_box.Add(self.header, flag = wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM, 
 			border = 10)
 
-		'''play_board is a container where all the tiles are put '''
-		play_board = wx.GridSizer(SIZE, SIZE)
+	def generate_playboard(self):
 		'''Set a list to store the numbers appear in different labels'''
 		tile_list = []
-		'''Get tiles information frome board'''
-
-		tile_list = board_object.get_tiles()
 		text_list = []
-		for i in range(0, SIZE):
-			for j in range(0, SIZE):
+		'''Get tiles information frome board'''
+		tile_list = self.board_object.get_tiles()
+		for i in range(0, self.SIZE):
+			for j in range(0, self.SIZE):
 				if tile_list[i][j] == None:
 					text_list.append('_')
 				else:
 					text_list.append(str(tile_list[i][j]))
-		'''This list is used to store the wx labels with information
-		ST.GenStaticText(self, -1, label = text_list[i])
-		And put all the numbers from the board into the GUI'''
-		label_list = []
-		for i in range(0, SIZE * SIZE):
-			label_list.append(ST.GenStaticText(self, -1, label = text_list[i],
-				size = (60, 30), style = wx.ALIGN_CENTRE))
-			label_list[i].SetBackgroundColour((238, 228, 218))
-			play_board.Add(label_list[i], flag = wx.EXPAND|wx.RIGHT|wx.TOP, border = 10)
 
-		panel_box.Add(play_board, flag = wx.EXPAND|wx.TOP|wx.LEFT, 
+		for i in range(0, self.SIZE * self.SIZE):
+			self.label_list.append(ST.GenStaticText(
+				self, 
+				-1, 
+				label = text_list[i],
+				size = (60, 30), 
+				style = wx.ALIGN_CENTRE))
+			self.label_list[i].SetBackgroundColour((238, 228, 218))
+			self.play_board.Add(self.label_list[i], flag = wx.EXPAND|wx.RIGHT|wx.TOP, border = 10)
+
+		self.panel_box.Add(
+			self.play_board, 
+			flag = wx.EXPAND|wx.TOP|wx.LEFT, 
 			border = 10)
 
-		'''User can use these keys to control the move and merge of the tile numbers'''
-		ctrl_keys = wx.BoxSizer(wx.VERTICAL)
+	def generate_ctrlkeys(self):
+		self.up_box = wx.BoxSizer()
+		self.up_button = wx.Button(self, -1, label = 'UP', size = (60, 30))
+		self.up_button.Bind(wx.EVT_BUTTON, self.up_button_click)
+		self.up_button.SetToolTip(wx.ToolTip("Click to move up"))
+		self.up_box.Add(self.up_button, flag = wx.EXPAND|wx.LEFT, border = 110)
+		self.ctrl_keys.Add(self.up_box)
 
-		up_box = wx.BoxSizer()
-		up_button = wx.Button(self, -1, label = 'UP', size = (60, 30))
-		up_box.Add(up_button, flag = wx.EXPAND|wx.LEFT, border = 110)
-		ctrl_keys.Add(up_box)
+		self.left_right_box = wx.GridSizer(1, 2)
+		self.left_button = wx.Button(self, -1, label = 'LEFT', size = (60, 30))
+		self.left_button.Bind(wx.EVT_BUTTON, self.left_button_click)
+		self.left_button.SetToolTip(wx.ToolTip("Click to move left"))
+		self.right_button = wx.Button(self, -1, label = 'RIGHT', size = (60, 30))
+		self.right_button.Bind(wx.EVT_BUTTON, self.right_button_click)
+		self.right_button.SetToolTip(wx.ToolTip("Click to move right"))
+		self.left_right_box.Add(self.left_button, flag = wx.LEFT, border = 80)
+		self.left_right_box.Add(self.right_button, flag = wx.RIGHT)
+		self.ctrl_keys.Add(self.left_right_box)
 
-		left_right_box = wx.GridSizer(1, 2)
-		left_button = wx.Button(self, -1, label = 'LEFT', size = (60, 30))
-		right_button = wx.Button(self, -1, label = 'RIGHT', size = (60, 30))
-		left_right_box.Add(left_button, flag = wx.LEFT, border = 80)
-		left_right_box.Add(right_button, flag = wx.RIGHT)
-		ctrl_keys.Add(left_right_box)
+		self.down_box = wx.BoxSizer()
+		self.down_button = wx.Button(self, -1, label = 'DOWN', size = (60, 30))
+		self.down_button.Bind(wx.EVT_BUTTON, self.down_button_click)
+		self.down_button.SetToolTip(wx.ToolTip("Click to move down"))
+		self.down_box.Add(self.down_button, flag = wx.EXPAND|wx.LEFT, border = 110)
+		self.ctrl_keys.Add(self.down_box)
 
-		down_box = wx.BoxSizer()
-		down_button = wx.Button(self, -1, label = 'DOWN', size = (60, 30))
-		down_box.Add(down_button, flag = wx.EXPAND|wx.LEFT, border = 110)
-		ctrl_keys.Add(down_box)
+		self.panel_box.Add(self.ctrl_keys, flag = wx.EXPAND|wx.ALIGN_CENTRE|wx.TOP, border = 10)
 
+	def refresh(self):
+		'''Set a list to store the numbers appear in different labels'''
+		tile_list = []
+		text_list = []
+		'''Get tiles information frome board'''
+		tile_list = self.board_object.get_tiles()
+		for i in range(0, self.SIZE):
+			for j in range(0, self.SIZE):
+				if tile_list[i][j] == None:
+					text_list.append('_')
+				else:
+					text_list.append(str(tile_list[i][j]))
 
-		panel_box.Add(ctrl_keys, flag = wx.EXPAND|wx.ALIGN_CENTRE|wx.TOP, border = 10)
+		for i in range(0, self.SIZE * self.SIZE):
+			self.label_list[i].SetLabel(text_list[i])
+		self.play_board.Layout()
+		self.panel_box.Layout()
 
-		self.SetSizer(panel_box)
-		self.Show(True)
+	def up_button_click(self, event):
+		board_object.move('up')
+		self.refresh()
+
+	def down_button_click(self, event):
+		board_object.move('down')
+		self.refresh()
+
+	def left_button_click(self, event):
+		board_object.move('left')
+		self.refresh()
+
+	def right_button_click(self, event):
+		board_object.move('right')
+		self.refresh()
 
 if __name__ == "__main__":
 	app = wx.App()
