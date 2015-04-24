e7e0ee4465a513434690c5ade5714e5305e7fdfb
serenafr <angneszhy@gmail.com>
2015-04-24 11:47:20 -0700

    The header can show the current score, the best score and start a new game

--- src/main_window.py
+++ src/main_window.py
@@ -1,18 +1,22 @@
 import wx
 import wx.lib.stattext as ST
 import board
+import score_board
 
 class My2048_wx(wx.Frame):
-	SIZE = 4
 
-	def __init__(self, parent, id, title, size, board_object):
+	def __init__(self, parent, id, title, size, board_object, score_board_object):
 		super(My2048_wx, self).__init__(parent, title = title, 
 			size = size)
 		self.board_object = board_object
+		self.score_board_object = score_board_object
+		self.SIZE = self.board_object.BOARD_SIZE
 		'''This list is used to store the wx labels with information
 		ST.GenStaticText(self, -1, label = text_list[i])
 		And put all the numbers from the board into the GUI'''
-		self.label_list = []
+		self.__label_list = []
+		self.__best_score = 0
+		self.__current_score = 0
 		self.Construct()
 
 	def Construct(self):
@@ -50,7 +54,7 @@ class My2048_wx(wx.Frame):
 		self.upper_header_score = wx.BoxSizer(wx.VERTICAL)
 		self.score_str = ST.GenStaticText(self, -1, label = 'SCORE', size = (50, 20), style = wx.ALIGN_CENTRE)
 		self.score_str.SetBackgroundColour((187, 173, 160))
-		self.score = ST.GenStaticText(self, -1, label = '0', size = (50, 20), style = wx.ALIGN_CENTRE)
+		self.score = ST.GenStaticText(self, -1, label = str(self.__current_score), size = (50, 20), style = wx.ALIGN_CENTRE)
 		self.score.SetForegroundColour('white')
 		self.score.SetBackgroundColour((187, 173, 160))
 		self.upper_header_score.AddMany([self.score_str, self.score])
@@ -59,7 +63,8 @@ class My2048_wx(wx.Frame):
 		self.upper_header_best = wx.GridSizer(2, 1)
 		self.best_str = ST.GenStaticText(self, -1, label = 'BEST', size = (50, 20), style = wx.ALIGN_CENTRE)
 		self.best_str.SetBackgroundColour((187, 173, 160))
-		self.best = ST.GenStaticText(self, -1, label = '0', size = (50, 20), style = wx.ALIGN_CENTRE)
+		self.__best_score = score_board_object.get_best_score()
+		self.best = ST.GenStaticText(self, -1, label = str(self.__best_score), size = (50, 20), style = wx.ALIGN_CENTRE)
 		self.best.SetForegroundColour('white')
 		self.best.SetBackgroundColour((187, 173, 160))
 		self.upper_header_best.AddMany([self.best_str, self.best])
@@ -74,6 +79,7 @@ class My2048_wx(wx.Frame):
 		self.lower_header.Add(self.sub_title, flag = wx.EXPAND|wx.RIGHT, border = 5)
 
 		self.new_game_button = wx.Button(self, -1, label = 'NEW GAME')
+		self.new_game_button.Bind(wx.EVT_BUTTON, self.new_game_button_click)
 		self.lower_header.Add(self.new_game_button)
 
 		self.header.Add(self.lower_header)
@@ -95,19 +101,17 @@ class My2048_wx(wx.Frame):
 					text_list.append(str(tile_list[i][j]))
 
 		for i in range(0, self.SIZE * self.SIZE):
-			self.label_list.append(ST.GenStaticText(
+			self.__label_list.append(ST.GenStaticText(
 				self, 
 				-1, 
 				label = text_list[i],
 				size = (60, 30), 
 				style = wx.ALIGN_CENTRE))
-			self.label_list[i].SetBackgroundColour((238, 228, 218))
-			self.play_board.Add(self.label_list[i], flag = wx.EXPAND|wx.RIGHT|wx.TOP, border = 10)
+			self.__label_list[i].SetBackgroundColour((238, 228, 218))
+			self.play_board.Add(self.__label_list[i], flag = wx.EXPAND|wx.RIGHT|wx.TOP, border = 10)
 
 		self.panel_box.Add(
-			self.play_board, 
-			flag = wx.EXPAND|wx.TOP|wx.LEFT, 
-			border = 10)
+			self.play_board, flag = wx.EXPAND|wx.TOP|wx.LEFT, border = 10)
 
 	def generate_ctrlkeys(self):
 		self.up_box = wx.BoxSizer()
@@ -138,6 +142,14 @@ class My2048_wx(wx.Frame):
 		self.panel_box.Add(self.ctrl_keys, flag = wx.EXPAND|wx.ALIGN_CENTRE|wx.TOP, border = 10)
 
 	def refresh(self):
+		self.__current_score = board_object.get_score()
+		self.score.SetLabel(str(self.__current_score))
+		self.__best_score = score_board_object.get_best_score()
+		if self.__current_score > self.__best_score:
+			self.__best_score = self.__current_score
+		self.best.SetLabel(str(self.__best_score))
+		self.upper_header.Layout()
+
 		'''Set a list to store the numbers appear in different labels'''
 		tile_list = []
 		text_list = []
@@ -151,28 +163,39 @@ class My2048_wx(wx.Frame):
 					text_list.append(str(tile_list[i][j]))
 
 		for i in range(0, self.SIZE * self.SIZE):
-			self.label_list[i].SetLabel(text_list[i])
+			self.__label_list[i].SetLabel(text_list[i])
 		self.play_board.Layout()
-		self.panel_box.Layout()
 
 	def up_button_click(self, event):
-		board_object.move('up')
+		if board_object.can_move():
+			board_object.move('up')
 		self.refresh()
 
 	def down_button_click(self, event):
-		board_object.move('down')
+		if board_object.can_move():
+			board_object.move('down')
 		self.refresh()
 
 	def left_button_click(self, event):
-		board_object.move('left')
+		if board_object.can_move():
+			board_object.move('left')
 		self.refresh()
 
 	def right_button_click(self, event):
-		board_object.move('right')
+		if board_object.can_move():
+			board_object.move('right')
 		self.refresh()
+
+	def new_game_button_click(self, event):
+		board_object.board_data = board_object.initialize_board(2)
+		self.refresh()
+		self.__current_score = 0
+		self.score.SetLabel(str(self.__current_score))
+		self.upper_header.Layout()
 
 if __name__ == "__main__":
 	app = wx.App()
 	board_object = board.Board(2)
-	frame = My2048_wx(None, -1, '2048', (380, 420), board_object)
+	score_board_object = score_board.Score_Board()
+	frame = My2048_wx(None, -1, '2048', (380, 420), board_object, score_board_object)
 	app.MainLoop()
