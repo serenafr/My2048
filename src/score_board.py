384482096884dbf9f711c78429ea15751577385f
serenafr <angneszhy@gmail.com>
2015-04-27 15:13:12 -0700

    Delete Score class in score_body.py, use score_board to store pure numbers, no more user's names.

--- src/score_board.py
+++ src/score_board.py
@@ -1,36 +1,23 @@
 import os.path
 import json
 
-class Score(object):
-	def __init__(self, name, score):
-		self.name = name
-		self.score = score
-
-	def __repr__(self):
-		return '%s: %d' % (self.name, self.score)
-
-	def __eq__(self, other):
-		return self.name == other.name and self.score == other.score
-
-
 class Score_Board(object):
 	def __init__(self):
 		self.__score_list = []
 
 	def sort_list(self):
-		self.__score_list.sort(lambda x, y: x.score - y.score, reverse = True)
+		self.__score_list.sort(lambda x, y: x - y, reverse = True)
 
-	def add_score(self, num, name):
+	def add_score(self, num):
 		if len(self.__score_list) < 10:
-			new_score = Score(name, num)
-			self.__score_list.append(new_score)
+			self.__score_list.append(num)
 			self.sort_list()
-		elif num > self.__score_list[len(self.__score_list) - 1].score:
-			self.__score_list[len(self.__score_list) - 1].score = num
-			self.__score_list[len(self.__score_list) - 1].name = name
+		elif num > self.__score_list[len(self.__score_list) - 1]:
+			self.__score_list[len(self.__score_list) - 1] = num
 			self.sort_list()
 
 	def get_scores(self):
+	#	self.load_scores(path)
 		n_list = self.__score_list
 		return n_list	
 
@@ -38,7 +25,7 @@ class Score_Board(object):
 		if len(self.__score_list) == 0:
 			best = 0
 		else:
-			best = self.__score_list[0]
+			best = self.get_scores()[0]
 		return best
 
 	def save_scores(self, path):
@@ -46,7 +33,7 @@ class Score_Board(object):
 		if not os.path.exists(dir_name):
 			os.makedirs(dir_name)
 		f = open(path, 'w')
-		jsonobj = [{"name" : i.name, "score" : i.score} for i in self.__score_list]
+		jsonobj = [i for i in self.__score_list]
 		jsonstr = json.dumps(jsonobj)
 		f.write(jsonstr)
 		f.close()
@@ -58,4 +45,4 @@ class Score_Board(object):
 		except IOError:
 			return
 		jsonobj = json.load(f)
-		self.__score_list = [Score(i["name"], i["score"]) for i in jsonobj]
+		self.__score_list = [i for i in jsonobj]
